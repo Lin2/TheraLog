5073948ae8982fcd3596d51b14134f30e54ba391
Amy He <39035908+Amyh11325@users.noreply.github.com>
2021-04-10 12:51:05 -0700

    read data and remove privacy policy

--- journalTest.js
+++ journalTest.js
@@ -16,14 +16,26 @@ function writeUserData(userId) {
             }
         },
         relation: {
-            r1: "BA0b65sL48gO8EIK0VckwL7Ukcq1"
+            r1: "Ri0I74dAeiSzv7Alb71SoUj4swI3"
         }
     });*/
   }
 
+  function outputEntry(userId, entryNum) {
+    var entryRef = firebase.database().ref('users/' + userId + '/entries/e' + entryNum);
+    entryRef.on('value', (snapshot) => {
+        if (snapshot.exists()) {
+            const data = snapshot.val();
+            document.body.innerHTML = data.date + ': ' + data.entry;
+        }
+    });
+  }
+
   firebase.auth().onAuthStateChanged(user => {
     if (user) {
-      writeUserData(user.uid);
+        writeUserData(user.uid);
+      outputEntry(user.uid, 1);
+      console.log(user.displayName);
     }
     else {
         console.log("error, user not logged in");
