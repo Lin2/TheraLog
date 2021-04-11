47552389e976e4cff6d3c9eaae6414adfe801aea
Amy He <39035908+Amyh11325@users.noreply.github.com>
2021-04-11 01:33:53 -0700

    read from database and buttons

--- therapistmain.js
+++ therapistmain.js
@@ -1,49 +1,61 @@
 //testing functions for main therapist
-function getPatients() {
-    var names = [];
+var names = [];
+var uid = [];
+function getPatients(callback) {
     var entryRef = firebase.database().ref('users/');
     entryRef.on('value', (snapshot) => {
-      if (snapshot.exists()) {
-        const data = snapshot.val();
-        for (var key in data) {
-          if (data[key].hasOwnProperty("isPatient") && data[key].isPatient === true) {
-            names.push(data[key].name);
-          }
+        if (snapshot.exists()) {
+            const data = snapshot.val();
+            for (var key in data) {
+                if (data[key].hasOwnProperty("isPatient") && data[key].isPatient === true) {
+                    names.push(data[key].name);
+                    uid.push(key);
+                }
+            }
         }
-      }
+        callback();
     });
-    return names;
-  }
-  
-  function setName(userId, nameData) {
+}
+
+function setName(userId, nameData) {
     firebase.database().ref('users/' + userId).update({
-      name: nameData
+        name: nameData
     });
-  }
-  
-  function start() {
-  firebase.auth().onAuthStateChanged(user => {
-    if (user) {
-      setName(user.uid, user.displayName);
-      firebase.database().ref('users/' + user.uid).on('value', (snapshot) =>{
-        const data = snapshot.val();
-        if (!data.hasOwnProperty("isPatient") || data.isPatient) {
-          window.location.href = "main.html";
-        } else {
-            document.getElementById("temp-hidden").style.visibility = "inherit";
-            document.getElementById("temp-hidden").style.visibility = "visible";
+}
+
+function start() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            setName(user.uid, user.displayName);
+            firebase.database().ref('users/' + user.uid).on('value', (snapshot) => {
+                const data = snapshot.val();
+                if (!data.hasOwnProperty("isPatient") || data.isPatient) {
+                    window.location.href = "main.html";
+                } else {
+                    getPatients(setTable);
+                    document.getElementById("temp-hidden").style.visibility = "inherit";
+                    document.getElementById("temp-hidden").style.visibility = "visible";
+                }
+            });
         }
-      });
-    }
-    else {
-      console.log("error, user not logged in");
-    }
-  })
+        else {
+            console.log("error, user not logged in");
+        }
+    })
 }
 
 function setTable() {
-    var names = getPatients();
+    var table = document.getElementById("table-with-names");
+    console.log(uid);
     
+    for(const key in names) {
+        table.innerHTML += "<tr> <td class = \"table-item\">" + names[key]+ "</td> <td class = \"table-button\" onclick=\"return goToMainPage('"+uid[key]+"');\">View Log</td> <td class = \"table-button\">Call</td> </tr>"
+    }
+}
+
+function goToMainPage(id) {
+    localStorage.setItem("id",id);
+    window.location.href = "main.html";
 }
 
 start();
